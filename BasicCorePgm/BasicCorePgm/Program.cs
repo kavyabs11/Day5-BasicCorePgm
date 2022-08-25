02a26b48a8b1deb391eb14d16c69ab847d80b71e
kavyabs11 <kavyasomshekar11@gmail.com>
2022-08-25 16:19:42 +0530

    [kavya] Add: finding quotient and reminder

--- BasicCorePgm/BasicCorePgm/Program.cs
+++ BasicCorePgm/BasicCorePgm/Program.cs
@@ -1,6 +1,6 @@
 ﻿using BasicCorePgm;
 Console.WriteLine("Welcome to basic core program");
-Factors basic = new Factors();
-Console.WriteLine($"Enter any number within 31");
-int a = Convert.ToInt32(Console.ReadLine());
-basic.Core();
+QutandRem basic = new QutandRem();
+basic.core();
+
+
---
+++ BasicCorePgm/BasicCorePgm/QutandRem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePgm
+{
+    public class QutandRem
+    {
+        public void core()
+        {
+            Console.WriteLine("Enter two numbers to find remainder and quotient");
+            int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+            if ((a > 0) && (b > 0))
+            {
+                Console.WriteLine($"The quotient is {a/b}");
+                Console.WriteLine($"The remainder is {a % b}");
+            }
+            else
+            {
+                Console.WriteLine("Invalud input, negative integers are not allowed");
+            }
+        }
+    }
+}
+   
