aab3a18147495df778aa6982d86bdd68050b8b2d
kavyabs11 <kavyasomshekar11@gmail.com>
2022-08-25 16:00:07 +0530

    [kavya] Add: power of 2

---
+++ BasicCorePgm/BasicCorePgm/Powerof2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePgm
+{
+    internal class Powerof2
+    {
+        public void Core(int n)
+
+        {
+            int limit = 31, num = 0;
+            if (n <= limit)
+            {
+                while (num <= n)
+                {
+                    double power = Math.Pow(2, num);
+                    Console.WriteLine(power);
+                    num++;
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Invalid input, limit is {limit}");
+            }
+        }
+    }
+}
+
--- BasicCorePgm/BasicCorePgm/Program.cs
+++ BasicCorePgm/BasicCorePgm/Program.cs
@@ -1,11 +1,6 @@
-﻿namespace BasicCorePgm
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-           LeapYear leap = new LeapYear();
-            leap.Core();
-        }
-    }
-}
+﻿using BasicCorePgm;
+Console.WriteLine("Welcome to basic core program");
+Powerof2 basic = new Powerof2();
+Console.WriteLine($"Enter any number within 31");
+int a = Convert.ToInt32(Console.ReadLine());
+basic.Core(a);
